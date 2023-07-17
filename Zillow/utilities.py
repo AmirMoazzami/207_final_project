e2274fc425b113d695f433b8d20c3d4cb4ededd2
Gray <112455496+gray-selby@users.noreply.github.com>
2023-07-16 22:46:59 -0400

    feature exploration

--- Zillow/utilities.py
+++ Zillow/utilities.py
@@ -21,6 +21,32 @@ class ZillowData:
     def __init__(self) -> None:
         self.folderPath = r"C:\Users\grays\OneDrive\Berkeley DS\Courses\207 ML\Final Project\Zillow Dataset"
 
+    def get_data(self):
+        """
+        properties_2016.csv
+        properties_2017.csv
+        train_2016_v2.csv
+        train_2017.csv
+        """
+        properties_2016 = pd.read_csv(
+            os.path.join(self.folderPath, "properties_2016.csv")
+        )
+        properties_2017 = pd.read_csv(
+            os.path.join(self.folderPath, "properties_2017.csv")
+        )
+        train_2016 = pd.read_csv(os.path.join(self.folderPath, "train_2016_v2.csv"))
+        train_2017 = pd.read_csv(os.path.join(self.folderPath, "train_2017.csv"))
+
+        train_2016 = pd.merge(train_2016, properties_2016, how="left", on="parcelid")
+        train_2017 = pd.merge(train_2017, properties_2017, how="left", on="parcelid")
+
+        all_properties = pd.concat(
+            [properties_2016, properties_2017], ignore_index=True
+        )
+        all_training = pd.concat([train_2016, train_2017], ignore_index=True)
+
+        return all_properties, all_training
+
     def get_train_2016(self):
         """"""
         answers_df = pd.read_csv(
